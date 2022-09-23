34e81b21a44010456b58e40c303d8c388f650115
Dylan Luu <dylanluu@ucsb.edu>
2022-09-22 23:51:25 -0400

    Added first and last names

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -25,6 +25,12 @@ const validateSignup = [
       .exists({ checkFalsy: true })
       .isLength({ min: 6 })
       .withMessage('Password must be 6 characters or more.'),
+    check('firstName')
+      .exists({ checkFalsy: true })
+      .withMessage('Must have first name'),
+    check('lastName')
+      .exists({ checkFalsy: true })
+      .withMessage('Must have last name'),
     handleValidationErrors
   ];
 
@@ -32,8 +38,8 @@ router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { email, password, username } = req.body;
-      const user = await User.signup({ email, username, password });
+      const { email, password, username, firstName, lastName } = req.body;
+      const user = await User.signup({ email, username, password, firstName, lastName });
 
       await setTokenCookie(res, user);
 
