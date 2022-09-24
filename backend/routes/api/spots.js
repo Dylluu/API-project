56178e37bc5121bd084ffee51e2841275dc813d6
Dylan Luu <dylanluu@ucsb.edu>
2022-09-24 02:11:06 -0400

    Gets spots with avgRating

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,10 +3,31 @@ const { Spot, Review, SpotImage, sequelize, User, Booking, ReviewImage } = requi
 const { Op } = require('sequelize');
 const router = express.Router();
 
-router.get('/', async(req, res) => {
-    res.json({
-        message: 'Route is working'
+router.get('/', async(req, res, next) => {
+    const responseBody = {};
+    const responseSpots = []
+
+    const spots = await Spot.findAll({
+        raw: true
     })
+    console.log(spots)
+
+    for(let i = 0; i < spots.length; i++){
+        let review = await Review.findAll({
+            where: {
+                spotId: spots[i].id
+            },
+            attributes: [
+                [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']
+            ],
+            raw: true
+        })
+        console.log(review)
+        spots[i].avgRating = review[0].avgRating
+    }
+
+    responseBody.Spots = spots
+    res.json(responseBody)
 })
 
 module.exports = router
