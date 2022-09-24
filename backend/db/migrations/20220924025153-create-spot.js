84958941f7755bc39016c051428505ab918dfd28
Dylan Luu <dylanluu@ucsb.edu>
2022-09-24 19:18:55 -0400

    Updated address to be unique

--- backend/db/migrations/20220924025153-create-spot.js
+++ backend/db/migrations/20220924025153-create-spot.js
@@ -14,7 +14,8 @@ module.exports = {
       },
       address: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: false,
+        unique: true
       },
       city: {
         type: Sequelize.STRING,
