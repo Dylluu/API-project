feecc11a7eb857490154d1889c1b1d3e7b2a1ccd
Dylan Luu <dylanluu@ucsb.edu>
2022-09-27 00:51:13 -0400

    Testing edit booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -47,6 +47,37 @@ router.put('/:bookingId', authenticate, async(req, res, next) => {
         })
     }
 
+    const spot = await Spot.findByPk(booking.spotId)
+
+    const bookings = await spot.getBookings()
+
+    for(let i = 0; i < bookings.length; i++){
+        let jsonBookings = bookings[i].toJSON();
+        let start = jsonBookings.startDate;
+        let end = jsonBookings.endDate;
+
+        if(startDate == start || startDate == end || (startDate > start && startDate < end)){
+            res.status(403);
+            return res.json({
+                message: 'Sorry, this spot is already booked for the specified dates',
+                statusCode: 403,
+                errors: {
+                    startDate: 'Start date conflicts with an existing booking'
+                }
+            })
+        }
+        if(endDate == start || endDate == end || (endDate > start && endDate < end)){
+            res.status(403);
+            return res.json({
+                message: 'Sorry, this spot is already booked for the specified dates',
+                statusCode: 403,
+                errors: {
+                    endDate: 'End date conflicts with an existing booking'
+                }
+            })
+        }
+    }
+
     try{
         if(startDate < endDate){
         const updatedBooking = await booking.update({
