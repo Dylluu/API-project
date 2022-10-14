3a28d3e76b573e26319ee0564529725833ad5e5d
Dylan Luu <dylanluu@ucsb.edu>
2022-10-13 21:30:54 -0700

    added LoginForm

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,6 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-
-const rootReducer = combineReducers({
-});
+import sessionReducer from './session';
 
 let enhancer;
 
@@ -14,6 +12,10 @@ if (process.env.NODE_ENV === 'production') {
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
+
+const rootReducer = combineReducers({
+    session: sessionReducer
+});
 
 const configureStore = (preloadedState) => {
     return createStore(rootReducer, preloadedState, enhancer);
