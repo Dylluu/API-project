fbc160e4c06f4f4f8bf07ebfe44c9734391d49a4
Dylan Luu <dylanluu@ucsb.edu>
2022-10-17 22:02:22 -0400

    Fixing navbar styling

--- backend/db/seeders/20220924051123-spot-images.js
+++ backend/db/seeders/20220924051123-spot-images.js
@@ -19,7 +19,22 @@ module.exports = {
     },
     {
       spotId: 1,
-      url: 'Spot 1 Image 2',
+      url: 'https://a0.muscache.com/im/pictures/miso/Hosting-46677438/original/e1ea285e-f417-43e3-80ae-b1d6114c9d90.jpeg?im_w=480',
+      preview: false
+    },
+    {
+      spotId: 1,
+      url: 'https://a0.muscache.com/im/pictures/miso/Hosting-46677438/original/664e64c8-de81-41a2-b58e-776d08d29713.jpeg?im_w=480',
+      preview: false
+    },
+    {
+      spotId: 1,
+      url: 'https://a0.muscache.com/im/pictures/miso/Hosting-46677438/original/0cc597ae-7b11-4cd3-9252-b57a0bc4710d.jpeg?im_w=480',
+      preview: false
+    },
+    {
+      spotId: 1,
+      url: 'https://a0.muscache.com/im/pictures/miso/Hosting-46677438/original/39c768bf-9d1b-4fef-9247-2354472fd7ce.jpeg?im_w=480',
       preview: false
     },
     {
@@ -29,22 +44,47 @@ module.exports = {
     },
     {
       spotId: 2,
-      url: 'Spot 2 Image 2',
+      url: 'https://a0.muscache.com/im/pictures/33c9d717-bfef-4bd0-a621-7f6023850905.jpg?im_w=480',
       preview: false
     },
     {
       spotId: 2,
-      url: 'Spot 2 Image 3',
+      url: 'https://a0.muscache.com/im/pictures/638856db-fa95-4dbc-807d-a485b02e4880.jpg?im_w=480',
+      preview: false
+    },
+    {
+      spotId: 2,
+      url: 'https://a0.muscache.com/im/pictures/e88ba83b-8450-441b-a495-06c002128cd6.jpg?im_w=480',
+      preview: false
+    },
+    {
+      spotId: 2,
+      url: 'https://a0.muscache.com/im/pictures/a9e9d441-6480-43d9-805e-16fce0084475.jpg?im_w=720',
       preview: false
     },
     {
       spotId: 3,
-      url: 'https://a0.muscache.com/im/pictures/monet/Luxury-570973165437649140/original/5b3872b7-fc85-4e67-bd3b-dc69e298dae3?im_w=960',
+      url: 'https://a0.muscache.com/im/pictures/miso/Hosting-37494869/original/badbdc0a-29a9-45ac-9625-ab6f3d5a1ee4.jpeg?im_w=960',
       preview: true
     },
     {
       spotId: 3,
-      url: 'Spot 3 Image 2',
+      url: 'https://a0.muscache.com/im/pictures/87520d01-348b-4e98-9936-20c56afd1105.jpg?im_w=240',
+      preview: false
+    },
+    {
+      spotId: 3,
+      url: 'https://a0.muscache.com/im/pictures/a397b8f4-6bb8-44f0-b9f2-821ac6675011.jpg?im_w=240',
+      preview: false
+    },
+    {
+      spotId: 3,
+      url: 'https://a0.muscache.com/im/pictures/3d82d1d2-a626-4607-9dc9-71d46ae1a15d.jpg?im_w=1200',
+      preview: false
+    },
+    {
+      spotId: 3,
+      url: 'https://a0.muscache.com/im/pictures/fc49a8cd-c1bc-4e67-8612-6f734f717cdb.jpg?im_w=1200',
       preview: false
     }
    ])
--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -42,15 +42,27 @@ const SpotDetails = () => {
             <div className='spot-details-images'>
                 <div className='img-1-container'>
                     {!isLoaded && <div className='img-1'></div>}
-                    {isLoaded &&
+                    {isLoaded && spot.SpotImages[0] &&
                         <img className='img-1' alt='img1' src={spot.SpotImages[0].url} />
                     }
                 </div>
                 <div className='rest-images-container'>
-                    <div className='img-2'></div>
-                    <div className='img-3'></div>
-                    <div className='img-4'></div>
-                    <div className='img-5'></div>
+                    {!isLoaded && <div className='img-2'></div>}
+                    {isLoaded && spot.SpotImages[1] &&
+                        <img className='img-2' alt='img2' src={spot.SpotImages[1].url} />
+                    }
+                    {!isLoaded && <div className='img-3'></div>}
+                    {isLoaded && spot.SpotImages[2] &&
+                        <img className='img-3' alt='img3' src={spot.SpotImages[2].url} />
+                    }
+                    {!isLoaded && <div className='img-4'></div>}
+                    {isLoaded && spot.SpotImages[3] &&
+                        <img className='img-4' alt='img4' src={spot.SpotImages[3].url} />
+                    }
+                    {!isLoaded && <div className='img-5'></div>}
+                    {isLoaded && spot.SpotImages[4] &&
+                        <img className='img-5' alt='img5' src={spot.SpotImages[4].url} />
+                    }
                 </div>
             </div>
             <div>
