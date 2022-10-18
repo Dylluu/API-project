a0619024a307fd0ae53a2b592c5c1dba32dd63cb
Dylan Luu <dylanluu@ucsb.edu>
2022-10-18 14:39:25 -0400

    Made review form without stars

--- frontend/src/components/ReviewForm/index.js
+++ frontend/src/components/ReviewForm/index.js
@@ -11,16 +11,28 @@ function ReviewForm() {
     const user = useSelector(state => state.session.user);
     return (
         <div className='review-form-wrapper'>
-            <form>
-                    <div>
-                        <input
-                        type='text'
+            <form className='review-form-class'>
+                <div className='review-top-bar'>
+                    <span className='how-was-stay'>Tell Us About Your Stay</span>
+                </div>
+                <div className='star-rating-div'>
+                    <span className='star-rating-text'>Star Rating</span>
+                </div>
+                <div className='how-was-your-experience-div'>
+                            <span className='how-was-your-experience-text'>How was your experience?</span>
+                        </div>
+                    <div className='review-text-div'>
+                        <textarea
+                        id='review-text'
                         value={reviewText}
                         onChange={(e) => setReviewText(e.target.value)}
                         required
                         placeholder='Write review here'
                         className='review-text-input'
                         />
+                    </div>
+                    <div className='review-submit-div'>
+                        <button className='review-submit-button'>Submit</button>
                     </div>
             </form>
         </div>
