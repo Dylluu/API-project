970d20ce06ee261454f9ce20d4be8b4d517ce5d1
Dylan Luu <dylanluu@ucsb.edu>
2022-10-21 05:00:12 -0400

    Bottom bar and sticky

--- frontend/src/components/SpotsBrowser/index.js
+++ frontend/src/components/SpotsBrowser/index.js
@@ -20,6 +20,25 @@ const SpotsBrowser = () => {
             {isLoaded && spotsArray.map(spot => (
                 <SpotCard key={spot.id} spot={spot}/>
             ))}
+            <div className='bott-bar'>
+                <div className='bott-inner'>
+                    <div className='bott-left'>
+                        <span
+                        style={{fontWeight: '250', fontSize: '14px'}}
+                        >© 2022 Airbenbe, Inc.</span>
+                    </div>
+                    <div className='bott-right'>
+                        <a
+                        className='git-linked'
+                        style={{color: 'black', cursor: 'pointer'}}
+                        href='https://github.com/Dylluu/API-project' target='_blank'>Github</a>
+                        <a
+                        className='git-linked'
+                        style={{color: 'black', marginLeft: '20px', cursor: 'pointer'}}
+                        href='https://www.linkedin.com/in/dylan-luu-0a869b1b8/' target='_blank'>LinkedIn</a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
